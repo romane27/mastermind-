65e7cea7f15f38c40c0efa3414d0a59913b7eb9d
romane27 <quintinromane2799@gmail.com>
2020-02-12 14:33:28 +0100

    creation vue

---
+++ src/mastermind/vueClavier.java
@@ -0,0 +1,7 @@
+package mastermind;
+
+import java.awt.Panel;
+
+public class vueClavier extends Panel {
+
+}
---
+++ src/mastermind/vuePropositions.java
@@ -0,0 +1,8 @@
+package mastermind;
+
+import java.awt.Canvas;
+
+public class vuePropositions extends Canvas {
+	
+
+}
