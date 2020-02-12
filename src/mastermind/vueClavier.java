0c95526d4ec1bc18315b26eceea098ed7b9edf0e
romane27 <quintinromane2799@gmail.com>
2020-02-12 15:05:21 +0100

    sss

--- src/mastermind/vueClavier.java
+++ src/mastermind/vueClavier.java
@@ -1,7 +1,24 @@
 package mastermind;
 
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
+import java.awt.FlowLayout;
 import java.awt.Panel;
+import java.awt.event.ItemListener;
 
 public class vueClavier extends Panel {
+	Modele m;
+	Checkbox[] boutons = new Checkbox[m.couleur.length];
+
+	public vueClavier() {
+		this.setLayout(new FlowLayout());
+		CheckboxGroup cg=new CheckboxGroup();
+		for(int i = 0; i < m.couleur.length; i++)
+		{
+			this.boutons[i] = new Checkbox();
+			// pour avoir un controle sur le clic 
+			this.boutons[i].addItemListener(l);
+			this.add(this.boutons[i]);
+		}
 	
-}
+}}
