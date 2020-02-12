a124b5e91d8aece790947a6ab7c10d37a6f64483
Loic Loupias <lloupias@pcalb-mm0000.univ-jfc.fr>
2020-02-12 14:15:39 +0100

    enlevé les slash

--- src/mastermind/Modele.java
+++ src/mastermind/Modele.java
@@ -7,14 +7,14 @@ public class Modele {
 	public int N_TENTATIVES = 10;
 	public int difficulte = 4;
 	public enum etats {EN_COURS, GAGNE, PERDU};
-	/*public Modele (Color[] coul, int tentatives, int diffi, enum etat) {
+	public Modele (Color[] coul, int tentatives, int diffi, enum etat) {
 		this.couleur = coul;
 		this.N_TENTATIVES= tentatives;
 		this.difficulte= diffi;
 		this.etats=etat;
 		
 		
-	}*/
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
